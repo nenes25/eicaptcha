343becdeb808a55490cb03903d3e04d94116f2c1
hhennes <hhennes@advisa.fr>
2018-02-13 12:54:20 +0100

    - add fr translations

---
+++ translations/fr.php
@@ -0,0 +1,27 @@
+<?php
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{eicaptcha}prestashop>eicaptcha_3e87eed4786fe702ae961b55bbf02c6a'] = 'EiCaptcha';
+$_MODULE['<{eicaptcha}prestashop>eicaptcha_76870a3a596aa5fe3b836a20e53c7698'] = 'Ajout de Captcha sur le formulaire de contact et de création de compte';
+$_MODULE['<{eicaptcha}prestashop>eicaptcha_793b58515cefe26f6a3c5ab782460a69'] = 'Le mode Captcha doit être configuré';
+$_MODULE['<{eicaptcha}prestashop>eicaptcha_c888438d14855d7d96a2724ee9c306bd'] = 'Paramètres mis à jour';
+$_MODULE['<{eicaptcha}prestashop>eicaptcha_fabff9d49a682feeffead298a9801c2b'] = 'Configuration EiCaptcha';
+$_MODULE['<{eicaptcha}prestashop>eicaptcha_77f682c46c4c98d39cfb703d3606f505'] = 'Pour récupérer vos propres clés, merci de cliquer sur le lien suivant';
+$_MODULE['<{eicaptcha}prestashop>eicaptcha_2d6759f60f165404e274683029df8e5b'] = 'Clé de site';
+$_MODULE['<{eicaptcha}prestashop>eicaptcha_de7e39ae12eb7d5e08c84ff8739ee5fc'] = 'Merci de saisir la clé de site';
+$_MODULE['<{eicaptcha}prestashop>eicaptcha_532719b9b22c0444d8c65bdc6b80fa1e'] = 'Clé secrète';
+$_MODULE['<{eicaptcha}prestashop>eicaptcha_30c1139b1b654780ec1ef5c35323806d'] = 'Merci de saisir la clé secrète';
+$_MODULE['<{eicaptcha}prestashop>eicaptcha_8734cb06c440a837b751eb664be56f3c'] = 'Activer pour le formulaire de contact';
+$_MODULE['<{eicaptcha}prestashop>eicaptcha_00d23a76e43b46dae9ec7aa9dcbebb32'] = 'Activé';
+$_MODULE['<{eicaptcha}prestashop>eicaptcha_b9f5c797ebbf55adccdd8539a65a0241'] = 'Désactivé';
+$_MODULE['<{eicaptcha}prestashop>eicaptcha_4d43056cebf6d60747ad8064a941b8cd'] = 'Activer pour la création de compte';
+$_MODULE['<{eicaptcha}prestashop>eicaptcha_2f298dd749c5c927a272586189aecaa9'] = 'Forcer la langue';
+$_MODULE['<{eicaptcha}prestashop>eicaptcha_50db68af68d07f407b398f20788e6277'] = 'Code langue ( en-GB | fr | de | de-AT | ... ) - Laisser vide pour utiliser la langue du site par défaut';
+$_MODULE['<{eicaptcha}prestashop>eicaptcha_43ec71bfafdad1482bff90168f80531b'] = 'Pour voir la liste des codes langues disponibles consulter la page : https://developers.google.com/recaptcha/docs/language';
+$_MODULE['<{eicaptcha}prestashop>eicaptcha_d721757161f7f70c5b0949fdb6ec2c30'] = 'Thème';
+$_MODULE['<{eicaptcha}prestashop>eicaptcha_9914a0ce04a7b7b6a8e39bec55064b82'] = 'Light';
+$_MODULE['<{eicaptcha}prestashop>eicaptcha_a18366b217ebf811ad1886e4f4f865b2'] = 'Dark';
+$_MODULE['<{eicaptcha}prestashop>eicaptcha_c9cc8cce247e49bae79f15173ce97354'] = 'Sauvegarder';
+$_MODULE['<{eicaptcha}prestashop>eicaptcha_632ee8e447c5c09ca7577f9281cbb999'] = 'Merci de valider le captcha';
+$_MODULE['<{eicaptcha}prestashop>eicaptcha_2422cb22995f528cda5e7d40750c2a55'] = 'Merci de patienter pendant la validation du captcha';
+$_MODULE['<{eicaptcha}prestashop>hookdisplaycustomeraccountform_45d048c35e3cfd449dc0a1b503cd103a'] = 'Captcha';
