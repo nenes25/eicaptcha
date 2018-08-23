fda0680dc36f5235b8f0a577f84dcc07d62efa69
hhennes <hhennes@advisa.fr>
2018-08-23 13:45:05 +0200

    V 0.4.15

--- eicaptcha.php
+++ eicaptcha.php
@@ -37,7 +37,7 @@ class EiCaptcha extends Module
         $this->author = 'hhennes';
         $this->name = 'eicaptcha';
         $this->tab = 'front_office_features';
-        $this->version = '0.4.14';
+        $this->version = '0.4.15';
         $this->need_instance = 1;
 
         $this->bootstrap = true;
