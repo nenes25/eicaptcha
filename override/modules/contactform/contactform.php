0d5a68fdbf007da59dd2dcf178d6abe5bc987d3d
Hervé Hennes <nenes_fr@yahoo.fr>
2021-08-16 14:23:36 +0200

    #196 Improve Code quality : introduce phpcsfixer and phpstan Fix phpCsFixer

--- override/modules/contactform/contactform.php
+++ override/modules/contactform/contactform.php
@@ -4,7 +4,7 @@ class ContactformOverride extends Contactform
 {
     public function sendMessage()
     {
-       //Module Eicaptcha : Check captcha before submit
+        //Module Eicaptcha : Check captcha before submit
         Hook::exec('actionContactFormSubmitBefore');
         if (!sizeof($this->context->controller->errors)) {
             parent::sendMessage();
