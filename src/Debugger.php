7cd9a7eb3576dc939eb7e0df81c24ffa90e3bab2
Hervé Hennes <nenes_fr@yahoo.fr>
2023-04-08 20:28:55 +0200

    Fix #218 Harmonize and fix licence headers

--- src/Debugger.php
+++ src/Debugger.php
@@ -1,21 +1,18 @@
 <?php
 /**
- * 2007-2021 PrestaShop
- *
  * NOTICE OF LICENSE
  *
  * This source file is subject to the Academic Free License (AFL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
+ * that is bundled with this package in the file docs/licenses/LICENSE.txt.
  * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/afl-3.0.php
+ * https://opensource.org/licenses/afl-3.0.php
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
- * to license@prestashop.com so we can send you a copy immediately.
+ * to contact@h-hennes.fr so we can send you a copy immediately.
  *
- * @author    Hennes Hervé <contact@h-hennes.fr>
- * @copyright 2013-2021 Hennes Hervé
- * @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
- *  http://www.h-hennes.fr/blog/
+ * @author    Hervé HENNES <contact@h-hhennes.fr> and contributors / https://github.com/nenes25/eicaptcha
+ * @copyright since 2013 Hervé HENNES
+ * @license   https://opensource.org/licenses/AFL-3.0  Academic Free License ("AFL") v. 3.0
  */
 
 namespace Eicaptcha\Module;
