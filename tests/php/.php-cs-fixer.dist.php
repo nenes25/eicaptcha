c8de6746a7da4dfad946459df5f984b63870cb08
Hervé Hennes <nenes_fr@yahoo.fr>
2024-02-15 08:45:48 +0100

    Update dependencies

--- tests/php/.php-cs-fixer.dist.php
+++ tests/php/.php-cs-fixer.dist.php
@@ -4,9 +4,9 @@ $config = new PrestaShop\CodingStandards\CsFixer\Config();
 
 $config
     ->setUsingCache(true)
+    ->setCacheFile(__DIR__.'/.php-cs-fixer.cache')
     ->getFinder()
-    ->in(__DIR__)
-    ->exclude('vendor')
-    ->exclude('translations');
+    ->in(__DIR__.'/../../')
+    ->exclude(['vendor','translations','tests']);
 
 return $config;
